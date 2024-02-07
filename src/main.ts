1833f0b5bfd2f6bdf4f9ecb9816a42efac23fdb9
Yury Rivera <yrivera@applaudostudios.com>
2024-02-06 21:57:32 -0600

    primer commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { Performance } from './performance';
+import './style.css';
+
+// create a network
+const container = document.getElementById('other');
+Performance(container as HTMLCanvasElement);
+
+
+
+
